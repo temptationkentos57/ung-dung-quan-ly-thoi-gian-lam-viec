0cc6084564ef6771d06ecbc2641f80595307b0aa
temptationkentos57 <kreetlannoo@hotmail.com>
2025-08-16 17:36:04 +0200

    Add comment for database connection

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// Kết nối tới MongoDB
 mongoose.connect('mongodb://localhost/ung_dung_quan_ly_thoi_gian', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Đã kết nối tới MongoDB'))
   .catch(err => console.log(err));
